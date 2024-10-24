28fd7b59a8365b19c14651fce0bbc30b8134fbf0
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-10-24 22:55:24 +0530

    initial commit

--- frontend/src/pages/Projects/Projects.js
+++ frontend/src/pages/Projects/Projects.js
@@ -17,6 +17,9 @@ import chat5 from '../../assets/Project Images/chat5.jpeg'
 import chat6 from '../../assets/Project Images/chat6.jpeg'
 import chat7 from '../../assets/Project Images/chat7.jpeg'
 import chat8 from '../../assets/Project Images/chat8.jpeg'
+import news1 from '../../assets/Project Images/news1.jpg'
+import news2 from '../../assets/Project Images/news2.jpg'
+import news3 from '../../assets/Project Images/news3.jpg'
 
 
 
@@ -163,6 +166,56 @@ export default function Projects() {
                                         </div>
                                         <div className="ad-btn">
                                             <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/VGossip'>Source Code</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project-content col-md-6 col-lg-5 col-xxl-4 col-sm-12">
+                                <div className="card rounded project-card">
+                                    <div className="card-image">
+                                        <span className="card-notify-badge">FullStack</span>
+                                        <div id="carouselExampleIndicators2" className="carousel slide images" data-bs-ride="carousel">
+                                            <div className="carousel-indicators">
+                                                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                            </div>
+                                            <div className="carousel-inner">
+                                                <div className="carousel-item active">
+                                                    <img src={news1} className="d-block w-100" alt="pic" />
+                                                </div>
+                                                <div className="carousel-item">
+                                                    <img src={news2} className="d-block w-100" alt="pic" />
+                                                </div>
+                                                <div className="carousel-item">
+                                                    <img src={news3} className="d-block w-100" alt="pic" />
+                                                </div>
+                                            </div>
+                                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev" style={{ width: '20px' }}>
+                                                {/* <span className="carousel-control-prev-icon" aria-hidden="true" style={{ backgroundColor: '#89023E' }}></span> */}
+                                                <span className="visually-hidden">Previous</span>
+                                            </button>
+                                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next" style={{ width: '20px' }}>
+                                                {/* <span className="carousel-control-next-icon" aria-hidden="true" style={{ backgroundColor: '#89023E' }}></span> */}
+                                                <span className="visually-hidden">Next</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="card-image-overly">
+                                        <span className="card-detail-badge">React</span>
+                                        <span className="card-detail-badge">Bootstrap</span>
+                                        <span className="card-detail-badge">JavaScript</span>
+                                        <span className="card-detail-badge">RestAPI</span>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="ad-title">
+                                            <h5 className="p-name text-uppercase">
+                                                NewTidings News App
+                                            </h5>
+                                        </div>
+                                        <div className="ad-btn" id='ad3-btn'>
+                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/VGossip'>Source Code</a>
+                                            <a className='btn btn-danger' id='live' rel="noreferrer" target='_blank' href='https://anuragkapoor19.github.io/NewTidings'>Launch App</a>
                                         </div>
                                     </div>
                                 </div>
