dc44d3c593c2dc6307a9a36209ca0dea712738af
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-10-25 07:59:22 +0530

    some changes

--- frontend/src/pages/Projects/Projects.js
+++ frontend/src/pages/Projects/Projects.js
@@ -94,7 +94,7 @@ export default function Projects() {
                                             </h5>
                                         </div>
                                         <div className="ad-btn">
-                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/Foodies'>Source Code</a>
+                                            <a className='btn btn-primary' rel="noreferrer" target='_blank' href='https://github.com/AnuragKapoor19/Foodies'>Source Code</a>
                                         </div>
                                     </div>
                                 </div>
@@ -165,7 +165,7 @@ export default function Projects() {
                                             </h5>
                                         </div>
                                         <div className="ad-btn">
-                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/VGossip'>Source Code</a>
+                                            <a className='btn btn-primary' rel="noreferrer" target='_blank' href='https://github.com/AnuragKapoor19/VGossip'>Source Code</a>
                                         </div>
                                     </div>
                                 </div>
@@ -214,7 +214,7 @@ export default function Projects() {
                                             </h5>
                                         </div>
                                         <div className="ad-btn" id='ad3-btn'>
-                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/Portfolio'>Source Code</a>
+                                            <a className='btn btn-primary' rel="noreferrer" target='_blank' href='https://github.com/AnuragKapoor19/Portfolio'>Source Code</a>
                                             <a className='btn btn-danger' id='live' rel="noreferrer" target='_blank' href='https://anuragkapoor19.github.io/NewTidings'>Launch App</a>
                                         </div>
                                     </div>
