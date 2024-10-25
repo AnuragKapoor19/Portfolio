7edd37865481d05e948fc1917715bcaceae0c102
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-10-25 07:27:29 +0530

    Some Changes

--- frontend/src/pages/Projects/Projects.js
+++ frontend/src/pages/Projects/Projects.js
@@ -214,7 +214,7 @@ export default function Projects() {
                                             </h5>
                                         </div>
                                         <div className="ad-btn" id='ad3-btn'>
-                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/VGossip'>Source Code</a>
+                                            <a className='btn btn-primary' href='https://github.com/AnuragKapoor19/Portfolio'>Source Code</a>
                                             <a className='btn btn-danger' id='live' rel="noreferrer" target='_blank' href='https://anuragkapoor19.github.io/NewTidings'>Launch App</a>
                                         </div>
                                     </div>
